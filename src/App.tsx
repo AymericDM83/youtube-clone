9bad8967f55a3dcef569e6a257c500093f742572
AymericDM83 <aymericmaggio@gmail.com>
2024-08-15 18:21:03 +0200

    Video grid correction

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <div className="max-h-screen flex flex-col">
       <PageHeader />
       <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
-        <div className="">Sidebar</div>
+        {/* <div className="">Sidebar</div> */}
         <div className="overflow-x-hidden px-8 pb-4">
           <div className="sticky top-0 bg-white z-10 pb-4">
             <CategoryPills
@@ -20,12 +20,12 @@ function App() {
               onSelect={setSelectedCategory}
             />
           </div>
+          <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+            {videos.map((video) => (
+              <VideoGridItem key={video.id} {...video} />
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
-        {videos.map((video) => (
-          <VideoGridItem key={video.id} {...video} />
-        ))}
       </div>
     </div>
   );
