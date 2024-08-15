f1ac6147c635b4151ed2a63b7129e663df4dd684
AymericDM83 <aymericmaggio@gmail.com>
2024-08-15 16:26:23 +0200

    Add arrows btns + calcs of translate amount

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,15 @@ function App() {
     <div className="max-g-screen flex flex-col">
       <PageHeader />
       <div className="grid grid-cols-[auto; 1fr] flex-grow-1 overflow-auto">
-        <div className="">Sidebar</div>
-        <div className="sticky top-0 bg-white z-10 pb-4">
-          <CategoryPills
-            categories={categories}
-            selectedCategory={selectedCategory}
-            onSelect={setSelectedCategory}
-          />
+        <div className="overflow-x-hidden px-8 pb-4">
+          <div className="">Sidebar</div>
+          <div className="sticky top-0 bg-white z-10 pb-4">
+            <CategoryPills
+              categories={categories}
+              selectedCategory={selectedCategory}
+              onSelect={setSelectedCategory}
+            />
+          </div>
         </div>
       </div>
     </div>
