ba19e32171373868786eaa7f2b33aa0b162b89ad
AymericDM83 <aymericmaggio@gmail.com>
2024-08-15 16:31:50 +0200

    Sidebar div correction

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ import { categories } from "./data/home";
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
   return (
-    <div className="max-g-screen flex flex-col">
+    <div className="max-h-screen flex flex-col">
       <PageHeader />
-      <div className="grid grid-cols-[auto; 1fr] flex-grow-1 overflow-auto">
+      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+        <div className="">Sidebar</div>
         <div className="overflow-x-hidden px-8 pb-4">
-          <div className="">Sidebar</div>
           <div className="sticky top-0 bg-white z-10 pb-4">
             <CategoryPills
               categories={categories}
